71f42a4f8b6fdf44ff04f53d966cac6c93f1e45f
Michael Angelo Dorio <michaelangelo.dorio@gmail.com>
2024-06-16 22:37:27 +0800

    handling functions - person map

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -11,9 +11,16 @@ interface IProps{
 
 const List: React.FC<IProps> = ({ people }) => {
     return (
-        <div>
-            I am a list of person
-        </div>
+    <>
+        <h4>I am a list of person</h4>
+        <ul>
+            {people.map(person => {
+                    return (
+                    <li>{person.name}</li>
+                )
+            })}
+        </ul>
+    </>
     )
 }
 
